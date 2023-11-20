9bca15d6cb2c7b643dfc986e126a8c8adcd5e699
Smart developer <developer@yandex.ru>
2023-11-20 19:17:26 +0300

    работает, но в новом окне

--- main.py
+++ main.py
@@ -3,27 +3,24 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtGui import QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsEllipseItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsEllipseItem, QGraphicsScene, QGraphicsView
 
 
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Загрузка интерфейса из файла UI.ui
         uic.loadUi('UI.ui', self)
-
+        self.scene = QGraphicsScene()
+        self.view = QGraphicsView(self.scene)
+        self.view.show()
         self.button.clicked.connect(self.add_circle)
 
     def add_circle(self):
-        # Генерация случайных координат и диаметра для окружности
-        x = random.randint(0, self.view.width())
-        y = random.randint(0, self.view.height())
         diameter = random.randint(10, 100)
 
-        # Создание окружности и добавление её на сцену
-        circle = QGraphicsEllipseItem(x, y, diameter, diameter)
-        circle.setBrush(QColor(255, 255, 0))  # Желтый цвет
+        circle = QGraphicsEllipseItem(50, 50, diameter, diameter)
+        circle.setBrush(QColor(255, 255, 0))
         self.scene.addItem(circle)
 
 
